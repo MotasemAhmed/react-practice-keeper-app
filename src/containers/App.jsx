a36cb192f164c8c78a85a5bc1ad7a8011af67e33
Motasem94 <amotasem.ahmed@gmail.com>
2021-03-04 17:43:08 +0200

    420

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,21 @@
-import Login from "./Login";
-var isLoggedIn = true;
+import React, { useState } from "react";
+
 function App() {
+  const [state, setstate] = useState(0);
+
+  function increase() {
+    setstate(state + 1);
+  }
+
+  function decrease() {
+    setstate(state - 1);
+  }
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      <h1>{state}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
   );
 }
 
